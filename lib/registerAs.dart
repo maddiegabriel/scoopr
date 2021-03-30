4be09a415db32ffaedde9cf47063565e572614b9
maddiegabriel <maddie.gabes@gmail.com>
2021-03-29 23:41:01 -0400

    updated style of login and register as pages

--- lib/registerAs.dart
+++ lib/registerAs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scoopr/signIn.dart';
 import 'package:scoopr/signUp.dart';
 import 'package:scoopr/vendorSignUp.dart';
 
@@ -7,43 +8,89 @@ class RegisterAsPage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-        appBar: AppBar(
-          title: Text(''),
-          backgroundColor: Colors.indigoAccent,
-        ),
         // Moves text field above keyboard
         body: SingleChildScrollView(
             child: Center(
                 child: Column(
                     children: <Widget>[
-                      new Image(
-                        image: new AssetImage('assets/images/logo.png'),
-                        height: 120.0,
-                        width: 300.0,
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.indigoAccent,
+                      new Padding(
+                        padding: const EdgeInsets.only(top: 160.0, bottom: 45.0),
+                        child: Image(
+                          image: new AssetImage('assets/images/logo.png'),
+                          width: 260.0,
                         ),
-                        onPressed: (){
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => SignUpPage()),
-                          );
-                        },
-                        child: Text("Customer"),
+                      ),
+                      Text(
+                        "Register as:",
+                        style: TextStyle(
+                            color: Color(0xff7bc1e4),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30
+                        ),
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.only(top: 40.0, bottom: 20.0),
+                          child:
+                          ConstrainedBox(
+                            constraints: BoxConstraints.tightFor(width: 200, height: 60),
+                            child:
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  primary: Color(0xff9CB6E4),
+                                  textStyle: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                                onPressed: (){
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => SignUpPage()),
+                                  );
+                                },
+                                child: Text("CUSTOMER"),
+                              ),
+                          ),
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.only(top: 20.0, bottom: 40.0),
+                          child:
+                          ConstrainedBox(
+                              constraints: BoxConstraints.tightFor(width: 200, height: 60),
+                              child:
+                              ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Color(0xff9CB6E4),
+                                    textStyle: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold
+                                    ),
+                                  ),
+                                    onPressed: (){
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(builder: (context) => VendorSignUpPage()),
+                                      );
+                                    },
+                                    child: Text("VENDOR")
+                                ),
+                          ),
                       ),
                       ElevatedButton(
                           style: ElevatedButton.styleFrom(
-                            primary: Colors.indigoAccent,
+                            primary: Colors.white,
+                            shadowColor: Colors.white,
                           ),
                           onPressed: (){
                             Navigator.push(
                               context,
-                              MaterialPageRoute(builder: (context) => VendorSignUpPage()),
+                              MaterialPageRoute(builder: (context) => SignInPage()),
                             );
                           },
-                          child: Text("Vendor")
+                          child: Text(
+                            "Already have an account? Login.",
+                            style: TextStyle(color: Color(0xffcfa5e4)),
+                          )
                       )
                     ]
                 )
--- lib/signIn.dart
+++ lib/signIn.dart
@@ -10,47 +10,67 @@ class SignInPage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-        appBar: AppBar(
-          title: Text(''),
-          backgroundColor: Colors.indigoAccent,
-        ),
         // Moves text field above keyboard
         body: SingleChildScrollView(
           child: Center(
             child: Column(
               children: <Widget>[
-                new Image(
-                  image: new AssetImage('assets/images/logo.png'),
-                  height: 120.0,
-                  width: 300.0,
-                ),
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    labelText: "Email"
+                new Padding(
+                  padding: const EdgeInsets.only(top: 110.0, bottom: 30.0),
+                  child: Image(
+                    image: new AssetImage('assets/images/logo.png'),
+                    width: 260.0,
                   ),
                 ),
-                TextField(
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                    labelText: "Password"
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, top: 10.0, right: 20.0, bottom: 10.0),
+                  child: TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: "Email"
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, top: 10.0, right: 20.0, bottom: 30.0),
+                    child:
+                      TextField(
+                        controller: passwordController,
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                        ),
+                      ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(top: 10.0, bottom: 30.0),
+                    child:
+                      ConstrainedBox(
+                        constraints: BoxConstraints.tightFor(width: 200, height: 60),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0xff9CB6E4),
+                              textStyle: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold
+                              ),
+                          ),
+                          onPressed: (){
+                            context.read<AuthenticationService>().signIn(
+                              email: emailController.text.trim(),
+                              password: passwordController.text.trim()
+                            );
+                          },
+                          child: Text("LOG IN"),
+                        ),
                   ),
                 ),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    primary: Colors.indigoAccent,
-                  ),
-                  onPressed: (){
-                    context.read<AuthenticationService>().signIn(
-                      email: emailController.text.trim(),
-                      password: passwordController.text.trim()
-                    );
-                },
-                child: Text("Sign In"),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.indigoAccent,
+                    primary: Colors.white,
+                    shadowColor: Colors.white,
                   ),
                   onPressed: (){
                     Navigator.push(
@@ -58,7 +78,10 @@ class SignInPage extends StatelessWidget{
                       MaterialPageRoute(builder: (context) => RegisterAsPage()),
                     );
                   },
-                  child: Text("Sign Up")
+                  child: Text(
+                      "Need an account? Register.",
+                      style: TextStyle(color: Color(0xffcfa5e4)),
+                  )
                 )
               ]
             )
